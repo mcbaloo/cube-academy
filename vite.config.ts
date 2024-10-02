e9dbda303ac178578d998b264e20ee75ce017871
Balogun Micheal <balogunmicheal3@gmail.com>
2024-10-02 21:44:48 +0100

    update vite config base

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/cube-academy/",
+  // base: "/cube-academy/",
   plugins: [react()],
   resolve: {
     alias: {
