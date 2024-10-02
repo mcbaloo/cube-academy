2b76ec9eacf8fdb4cebf45d78103710b6517717f
Balogun Micheal <balogunmicheal3@gmail.com>
2024-10-02 21:29:21 +0100

    aded base for github pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { Component } from 'react'
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/cube-academy/",
   plugins: [react()],
   resolve: {
     alias: {
